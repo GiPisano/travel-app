613e2a1420d8078b49c723cedcbe1228814e1af5
GiPisano <pisanogiacomo9@gmail.com>
2024-08-05 17:49:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Web app per la pianificazione e gestione del tuo viaggio. Suddividi il viaggio in giornate in cui ogni giornata riporta le tappe da visitare. Inserisci dettagli (titolo, descrizione, data etc), immagini, cibo, curiosità o qualsiasi altra info.
+Le tappe del viaggio saranno anche visualizzate su mappa tramite un servizio a tua scelta.
+Implementa una funzionalità per tenere traccia della progressione delle tappe del tuo viaggio (anche quando chiudi la pagina).
+Aggiungi personalizzazioni e funzionalità a tuo piacimento. Alcuni esempi:
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script></script>
+
+<template>
+  <div class="container-app">
+    <nav></nav>
+    <main></main>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+body {
+  background-color: black;
+}
+
+.container-app {
+  margin: 30px;
+  display: flex;
+  gap: 1rem;
+  nav {
+    width: 30%;
+    height: calc(100vh - 60px);
+    border-radius: 2rem;
+    background-color: rgb(43, 43, 43);
+  }
+
+  main {
+    width: 70%;
+    height: calc(100vh - 60px);
+    border-radius: 2rem;
+    background-color: rgb(43, 43, 43);
+  }
+}
+</style>
